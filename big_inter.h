82fd325c771da07117805c3932c4b4ba330a3377
晋毓麟 <2522471070@qq.com>
2018-12-18 01:21:55 +0800

    big_inter

---
+++ big_inter.h
@@ -0,0 +1,35 @@
+//
+// Created by 晋毓麟 on 2018/12/18.
+//
+
+#ifndef CLION_BIG_INTER_H
+#define CLION_BIG_INTER_H
+
+#endif //CLION_BIG_INTER_H
+using namespace std;
+string big_add(string a,string b)
+{
+    string s;
+    reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    int sum = 0;
+    for (int i = 0;i<b.length();i++)
+    {
+        sum = (a[i]-'0')+(b[i]-'0')+sum;
+        s += char(sum%10 + '0');
+        sum/=10;
+    }
+    for (int i = b.length();i<a.length();i++)
+    {
+        sum = (a[i]-'0') + sum;
+        s += char(sum%10 + '0');
+        sum/=10;
+    }
+    while(sum > 0)
+    {
+        s += char(sum%10 + '0');
+        sum/=10;
+    }
+    reverse(s.begin(),s.end());
+    return s;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+//#include "big_inter_operator.cpp"
+#include "big_inter.h"
+using namespace std;
+//string big_add(string,string);
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout <<"你好"<<std::endl;
-    //注释测试
+    string a,b,c;
+    cin>>a>>b;
+    if (a.length()>b.length())
+        c = big_add(a,b);
+    else
+        c = big_add(b,a);
+    cout<<c<<endl;
+
     return 0;
 }
