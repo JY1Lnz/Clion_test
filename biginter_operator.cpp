bda3bf7302338557e424251e413d047c6f47d54b
晋毓麟 <2522471070@qq.com>
2018-12-18 16:48:03 +0800

    debug big_sub,big_add function now zhe bignumber add and sub is good;

--- biginter_operator.cpp
+++ biginter_operator.cpp
@@ -3,12 +3,13 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-string big_add(string &a,string &b)
+string big_add(string a,string b)
 {
     string s;
     reverse(a.begin(),a.end());
     reverse(b.begin(),b.end());
     int sum = 0;
+
     for (int i = 0;i<b.length();i++)
     {
         sum = (a[i]-'0')+(b[i]-'0')+sum;
@@ -26,11 +27,16 @@ string big_add(string &a,string &b)
         s += char(sum%10 + '0');
         sum/=10;
     }
+    for (int i = s.length()-1;i>=0;i--)
+        if (s[i] != '0')
+            break;
+        else
+            s[i] = 0;
     reverse(s.begin(),s.end());
     return s;
 }
 
-string big_sub(string &a,string &b)//忽视正负问题
+string big_sub(string a,string b)//忽视正负问题
 {
     string s;
     reverse(a.begin(),a.end());
@@ -51,15 +57,29 @@ string big_sub(string &a,string &b)//忽视正负问题
         }
         else
         {
+            overflow = 0;
             s += '0';
         }
     }
     for (int i = b.length();i<a.length();i++)
     {
         a[i] = a[i] - overflow;
-        overflow = 0;
-        s += a[i];
+        if ((a[i]-'0') < 0)
+        {
+            overflow = 1;
+            s += '9';
+        }
+        else
+        {
+            overflow = 0;
+            s += a[i];
+        }
     }
+    for (int i = s.length()-1;i>=0;i--)
+        if (s[i] != '0'||i == 0)
+            break;
+        else
+            s.erase(i);
     reverse(s.begin(),s.end());
     return s;
 }
