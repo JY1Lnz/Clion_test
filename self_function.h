8060d811683eab575280d0f1df3414fddb96a4dd
晋毓麟 <2522471070@qq.com>
2018-12-18 12:33:16 +0800

    create self_function

---
+++ self_function.h
@@ -0,0 +1,9 @@
+//
+// Created by 晋毓麟 on 2018/12/18.
+//
+using namespace std;
+#ifndef CLION_SELF_FUNCTION_H
+#define CLION_SELF_FUNCTION_H
+string big_add(string a,string b);
+
+#endif //CLION_SELF_FUNCTION_H
